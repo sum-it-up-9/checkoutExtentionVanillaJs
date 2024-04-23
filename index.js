fe089c1efc6078d49e12e50638ee924e9427303f
sumit962 <sumit.adsul@vivacommerce.com>
2024-04-23 16:52:56 +0530

    added chekout extention

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ checkoutKitLoader.load('extension').then(async function(module) {
   
 
   const orderId = params.get('orderId');
+  const cartId = params.get('cartId');
   const parentOrigin = params.get('parentOrigin');
 
   console.log('orderId: ',orderId);
+  console.log('cartId: ',cartId);
   console.log('parentOrigin: ',parentOrigin);
   console.log('extensionId: ',extensionId);
 
