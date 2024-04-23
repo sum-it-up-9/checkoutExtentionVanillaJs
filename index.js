6d9d8ced0b64b206fbaee310b40b9bd19088c9cc
sumit962 <sumit.adsul@vivacommerce.com>
2024-04-23 13:26:08 +0530

    modify html-2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 checkoutKitLoader.load('extension').then(async function(module) {
   console.log("Checkout loader - extension")
-  // const params = new URL(document.location).searchParams;
-  // const extensionId = params.get('extensionId');
-  // const cartId = params.get('cartId');
-  // const parentOrigin = params.get('parentOrigin');
-  // const extensionService = await module.initializeExtensionService({
-  //   extensionId,
-  //   parentOrigin,
-  //   taggedElementId: 'content',
-  // });
+  const params = new URL(document.location).searchParams;
+  const extensionId = params.get('extensionId');
+  const cartId = params.get('cartId');
+  const parentOrigin = params.get('parentOrigin');
+  const extensionService = await module.initializeExtensionService({
+    extensionId,
+    parentOrigin,
+    taggedElementId: 'content',
+  });
 
 
   
